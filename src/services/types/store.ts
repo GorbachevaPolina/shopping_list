7464422c9661ef37a62b483aadb52f1d271388a2
GorbachevaPolina <gorbacheva.polina7394@gmail.com>
2023-05-13 10:10:04 +0300

    initialised project

---
+++ src/services/types/store.ts
@@ -0,0 +1,14 @@
+import { ThunkAction } from 'redux-thunk';
+import { TypedUseSelectorHook, useDispatch as dispatchHook, useSelector as selectorHook } from 'react-redux';
+import { store } from '../..';
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type TApplicationActions = any;
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, TApplicationActions>
+export type AppDispatch = <TReturnType>(action: TApplicationActions | AppThunk) => TReturnType
+
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook; 
+
+export const useDispatch = () => dispatchHook<AppDispatch>(); 
